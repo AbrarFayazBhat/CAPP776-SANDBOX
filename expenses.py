54a69093f21050a18fc4daa565e70d901c74d335
Abrar Fayaz <bhatabrar517@gmail.com>
2026-08-29 21:11:22 +0530

    add expenses module

---
+++ expenses.py
@@ -0,0 +1,11 @@
+def morning_expen(m):
+    print(f"Morning expenses ={m}")
+
+def afernoon_expen(a):
+    print(f"Afternoon expenses ={a}")
+
+def evening_expen(e):
+    print(f"Evening expenses ={e}")
+
+def total_expen(m,a,e):
+    print(f"Total expenses={m+a+e}")
